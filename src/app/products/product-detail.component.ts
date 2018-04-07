92a4237454a1e3fa58e2901f44faca53fd7b7a32
JIntrocaso <jlintrocaso@gmail.com>
2018-04-07 14:23:40 -0400

    added functionality to get proper product on product detail page; added product info to detail page

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,32 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  errorMessage: any;
   pageTitle: string = 'Product Detail';
   product: IProduct;
   constructor(private _route: ActivatedRoute,
-              private _router: Router) { }
+              private _router: Router,
+              private _productService: ProductService) { }
 
   ngOnInit() {
     // tslint:disable-next-line:prefer-const
     let id = +this._route.snapshot.paramMap.get('id');
-    this.pageTitle += `: ${id}`;
-    this.product =     {
-      'productId': 1,
-      'productName': 'Leaf Rake',
-      'productCode': 'GDN-0011',
-      'releaseDate': 'March 19, 2016',
-      'description': 'Leaf rake with 48-inch wooden handle.',
-      'price': 19.95,
-      'starRating': 3.2,
-      'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
-  };
-
+    this._productService.getProduct(id)
+                          .subscribe(product => this.product = product,
+                                      error => this.errorMessage = <any>error);
   }
 
   onBack(): void {
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -4,20 +4,31 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
 
 import { IProduct } from './product';
+import { callLifecycleHooksChildrenFirst } from '@angular/core/src/view/provider';
 
 @Injectable()
 export class ProductService {
     private _productUrl = './api/products/products.json';
-
+    product: IProduct;
     constructor(private _http: HttpClient) {
 
     }
+
     getProducts(): Observable<IProduct[]> {
         return this._http.get<IProduct[]>(this._productUrl)
                     .do(data => console.log('All: ' + JSON.stringify(data)))
                     .catch(this.handleError);
+    }
+
+    getProduct(productId: number): Observable<IProduct> {
+        const id = productId;
+        return this.getProducts().map(products => {
+            return products.filter(item => item.productId === id)[0];
+        })
+            .catch(this.handleError);
     }
 
     private handleError(err: HttpErrorResponse) {
